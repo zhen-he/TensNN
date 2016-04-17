40b067dad5c5052d034b4c3847aac7b19a23ab77
Zhen He <hezhen13@qq.com>
2016-04-17 20:01:44 +0100

    Added files via upload

--- scripts/draw.py
+++ scripts/draw.py
@@ -1,15 +1,30 @@
 import json
 import matplotlib.pyplot as plt
-   
-with open('t3_256.json', 'r') as f1:
-  data1 = json.load(f1)
-hist1 = data1['val_loss_history']
 
-with open('checkpoint_4000.json', 'r') as f2:
+
+drawTrain = 1
+
+str = 'val'
+if drawTrain == 1: 
+  str = 'train'
+
+# with open('t3_256_lastBN.json', 'r') as f1:
+#   data1 = json.load(f1)
+# hist1 = data1[str + '_loss_history']
+ 
+with open('t3_256_lastBN.json', 'r') as f2:
   data2 = json.load(f2)
-hist2 = data2['val_loss_history']
+hist2 = data2[str + '_loss_history']
+
+with open('t32_256_BN.json', 'r') as f3:
+  data3 = json.load(f3)
+hist3 = data3[str + '_loss_history']
+
+# with open('t1_len100.json', 'r') as f4:
+#   data4 = json.load(f4)
+# hist4 = data4[str + '_loss_history']
 
 
-plt.plot(hist1, 'r-', hist2, 'g-')
+plt.plot(hist2, 'g-', hist3, 'b-')
 plt.ylabel('val_loss')
 plt.show()
